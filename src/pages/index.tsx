e37649008bcafd240ff5881989c01c91fbcaa326
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-10-20 16:53:01 +0900

    NextAuthでログイン認証、仮ページ遷移から置換

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,18 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
 
 const IndexPage = () => {
     const router = useRouter();
+    const { status } = useSession();
 
     useEffect(() => {
-        const checkLoginStatus = async () => {
-            // Replace this with your actual login check logic
-            const isLoggedIn = await fakeLoginCheck();
-
-            if (isLoggedIn) {
-                router.push('/mypage');
-            } else {
-                router.push('/login');
-            }
-        };
-
-        checkLoginStatus();
-    }, [router]);
-
-    // Fake login check function for demonstration purposes
-    const fakeLoginCheck = async () => {
-        // Simulate an API call
-        return new Promise((resolve) => setTimeout(() => resolve(true), 1000));
-    };
+        if (status === "authenticated") {
+            router.push('/mypage');
+        } else if (status === "unauthenticated") {
+            router.push('/login');
+        }
+    }, [status, router]);
 
     return null;
 };
